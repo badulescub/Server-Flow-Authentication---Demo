2663ea90b728e524e70b122a14949b514636f11d
bogdan.badulescu <bogdan.badulescu@bynapse.com>
2017-04-23 15:07:48 -0400

    Finalizing Authentication

--- Client/ClientApp/app/components/login/login.component.ts
+++ Client/ClientApp/app/components/login/login.component.ts
@@ -1,29 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Pipe, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { AzureService, AzureAuthenticationUser } from '../../services/mobile.service';
 
 @Component({
-  selector: 'login',
-  templateUrl: './login.component.html',
+    selector: 'login',
+    templateUrl: './login.component.html',
 })
-export class LoginComponent {
-  
-  loginForm: FormGroup;
-  forgotPasswordForm: FormGroup;
+export class LoginComponent implements OnInit {
 
-  loginFailed = false;
-  loginState = 'login';
+    loginForm: FormGroup;
+    loginFailed = false;
+    currentUser: any;
 
-  private azureServiceClient: any;
-  
-  constructor(
-    private formBuilder: FormBuilder,
-    private router: Router,
-    private azureService: AzureService
-  ) {
-      this.buildForm();
+
+    private azureServiceClient: any;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private router: Router,
+        private azureService: AzureService
+    ) {
+        this.buildForm();
+    }
+
+    ngOnInit() {
+        this.currentUser = this.azureService.isLoggedOn();
     }
 
     buildForm(): void {
@@ -42,47 +45,47 @@ export class LoginComponent {
 
     logout(): void {
         this.azureService.logout().then(() => {
+            this.currentUser = null;
             this.router.navigate(['', 'home']);
         });
     }
 
     loginWithFacebook(): void {
-        
-        this.azureService.login('facebook', null).then(() =>
-        {
+
+        this.azureService.login('facebook', null).then(() => {
             this.router.navigate(['', 'home']);
         });
     }
 
     loginWithGoogle(): void {
-        
+
         this.azureService.login('google', null).then(() => {
             this.router.navigate(['', 'home']);
         });
     }
 
     loginWithTwitter(): void {
-        
+
         this.azureService.login('twitter', null).then(() => {
             this.router.navigate(['', 'home']);
         });
     }
 
     loginWithUserName(value: any): void {
-        
+
         if (this.loginForm.invalid) return;
 
-         let user = new AzureAuthenticationUser();
-         user.userName = value.userName;
-         user.password = value.password;
+        let user = new AzureAuthenticationUser();
+        user.userName = value.userName;
+        user.password = value.password;
 
-         this.azureService.login('custom', user)
-        .then(() => {
-            this.router.navigate(['', 'home']);
-        })
-        .catch((reason) => {
-            console.log(reason);
-            this.loginFailed = true;
-        });
+        this.azureService.login('custom', user)
+            .then(() => {
+                this.router.navigate(['', 'home']);
+            })
+            .catch((reason) => {
+                console.log(reason);
+                this.loginFailed = true;
+            });
     }
 }
